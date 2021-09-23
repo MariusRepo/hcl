a4edb42a60b9df4233c188d308cbbaeccc7a1f56
Marius M <serious.m.work@gmail.com>
2021-09-23 12:25:53 +0300

    [INIT COMMIT] Initial commit

---
+++ src/main/java/hcl/hackathon/hcl/HCLApplication.java
@@ -0,0 +1,13 @@
+package hcl.hackathon.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HCLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HCLApplication.class, args);
+	}
+
+}
